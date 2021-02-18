4447330aa4b26dd1bf23b0762c75931be6c58f40
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-02-18 15:20:13 -0600

    uwu

---
+++ main.js
@@ -0,0 +1,58 @@
+function quest(string){
+return string + "???";
+};
+let result1 = quest("Hello");
+let result2 = quest("what even");
+result1;
+result2;
+
+function spaces(string, string1){
+    return string + " " + string1;
+};
+
+let res1 = spaces("oh","hi");
+res1;
+let res2 = spaces("well","hello back");
+res2;
+
+function subtract(num1,num2,num3){
+    sum = num1 - num2 - num3;
+    return sum;
+}
+
+sum1 = subtract(10,1,2);
+sum1;
+sum2 = subtract(8,3,5);
+sum2;
+sum3 = subtract(100,80,25);
+sum3;
+
+function subrev(num1,num2,num3){
+    summ = num3 - num2 - num1;
+    return summ
+};
+summ1 = subrev(10,1,2);
+summ1;
+summ2 = subrev(2,1,10);
+summ2;
+
+function tripple(string){
+    trippl = string+string+string;
+    return trippl;
+};
+
+trip1 = tripple("codeimmersives");
+trip1;
+trip2 = tripple("is the best");
+trip2;
+
+function square(num){
+    sum = Math.sqrt(num);
+    return sum;
+}
+task1 = square(16);
+task1;
+task2 = square(4);
+task2;
+task3 = square(9);
+task3;
